91c11068c72d34436c533a02c406a47431f2f966
Jeff Farr <jefarr@wayfair.com>
2023-01-06 15:59:08 -0500

    init commit

---
+++ default-puppet.pp
@@ -0,0 +1,23 @@
+# === Class: ${1:$$(yas-choose-value '("role" "profile" ""))}::$2
+#
+# $3
+#
+# === Parameters
+#
+# None
+#
+# === Examples
+#
+# include ::$1::$2
+#
+# === Authors
+#
+# Jeff Farr <jefarr@wayfair.com>
+#
+# === Copyright
+#
+# Copyright `(format-time-string "%Y")` Wayfair LLC -- All rights reserved.
+#
+class $1::$2 {
+  $0
+}
